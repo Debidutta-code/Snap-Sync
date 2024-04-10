d149df48229b1f9a324c2520017456298a5816fe
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-04-10 14:24:33 +0530

    loading animation change

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState, useRef } from "react";
 import VideoCard from "./VideoCard";
 import "./Homepage.css";
-import { PropagateLoader } from "react-spinners";
+import { PropagateLoader, BarLoader } from "react-spinners";
 
 const Homepage = () => {
   const [allVideos, setAllVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [allVideosLoaded, setAllVideosLoaded] = useState(false); // Flag to track if all videos are loaded
+  const [moreLoading, setMoreLoading] = useState(true);
   const videosPerPage = 9;
 
   const observer = useRef();
   const lastVideoRef = useRef();
 
   const fetchVideos = async () => {
+    setMoreLoading(true);
     try {
       const response = await fetch(
         `https://snap-sync-tau.vercel.app/get-videos?page=${currentPage}&limit=${videosPerPage}`
@@ -28,9 +30,11 @@ const Homepage = () => {
         setAllVideos((prevVideos) => [...prevVideos, ...data]);
       }
       setLoading(false);
+      setMoreLoading(false);
     } catch (error) {
       console.error("Error fetching videos:", error.message);
       setLoading(false);
+      setMoreLoading(false);
     }
   };
 
@@ -105,7 +109,13 @@ const Homepage = () => {
           }
         })}
       </div>
-      {/* {!loading && allVideos.length > 0 && allVideosLoaded && (
+      {moreLoading && (
+        <div className="loading-animation-home">
+          <BarLoader color="#999159" height={2} width={500} />
+        </div>
+      )}
+
+      {/* {!moreLoading && allVideos.length > 0 && allVideosLoaded && (
         <div className="home-no-videos-message-home">
           <h2>You've reached the end of the road, nothing left to discover!</h2>
         </div>
